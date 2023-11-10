dc4751488aa76a67ff713b6323ea72c363e3e1b2
Gabriel-Borges12 <borgesgs369@gmail.com>
2023-11-10 11:07:56 -0300

    commit

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +10,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
     Option 1: Include in HTML -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="icon" href=" ./img/logo.png">
     <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
 
@@ -35,22 +37,24 @@
             <!-- <img id="logoLoginDir" src="img/WEEK.png"> -->
             <!-- <h1 id="tituloDir">Bem vindo ao Week!</h1> -->
             <div class="logo-responsiva">
-    <img id="logologin" src="img/WEEK.png">
-</div>
+                <img id="logologin" src="img/WEEK.png">
+            </div>
             <div class="container">
 
                 <form id="cadastro-form" method="post" action="processar_login.php">
                     <h1 id="logintitulo">Login</h1>
                     <img id="img-user" src="img/user.png">
-                    <input type="text" class="formzao" name="user" placeholder="Nome do usuario" autocomplete = "off">
-                    <br> <br>
-                    <img id="img-senha" src="img/senha.png">
-    
-                    <input type="password" class="formzao" name="senha" placeholder="Senha" id="senha">
-                    <!-- Adicione o ícone do olho para mostrar/ocultar a senha -->
-                    <span class="lnr lnr-eye" id="show-password" style="cursor: pointer;"></span>
+                    
+                        <input type="text" class="formzao" name="user" placeholder="Nome do usuario" autocomplete="off"
+                            id="usuario">
+                        <br> <br>
+                        <img id="img-senha" src="img/senha.png">
 
-                   
+                        <input type="password" class="formzao" name="senha" placeholder="Senha" id="senha">
+                        <!-- Adicione o ícone do olho para mostrar/ocultar a senha -->
+                        <i class="fa-regular fa-eye" id="show-password"></i>
+                    
+
                     <p id=""><a class="esqueceur" href="redefinirsenha.php">Esqueceu sua senha?</a></p>
                     <!-- <input type="button" value="" href="home.html"> -->
                     <!-- <button type="button" id="botaologin" onclick="window.location.href='./home.php'">Login</button> -->
@@ -62,17 +66,17 @@
                     <!-- <input type="button" value="Click Me" onclick=""/> -->
 
                     <script>
-                         const senhaInput = document.getElementById("senha");
-                         const showPasswordIcon = document.getElementById("show-password");
+                        const senhaInput = document.getElementById("senha");
+                        const showPasswordIcon = document.getElementById("show-password");
 
-                         showPasswordIcon.addEventListener("click", () => {
-                        if (senhaInput.type === "password") {
-                        senhaInput.type = "text";
-                         } else {
-                        senhaInput.type = "password";
-                         }
-        });     
-    </script>
+                        showPasswordIcon.addEventListener("click", () => {
+                            if (senhaInput.type === "password") {
+                                senhaInput.type = "text";
+                            } else {
+                                senhaInput.type = "password";
+                            }
+                        });     
+                    </script>
                 </form>
             </div>
         </div>
